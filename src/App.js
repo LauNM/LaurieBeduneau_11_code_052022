14c49a8e07f2e0a824fbe5711d52e9570a6b94e5
LauNM <laurie.beduneau@gmail.com>
2022-06-01 14:42:10 +0200

    set project with data

--- src/App.js
+++ src/App.js
@@ -3,17 +3,40 @@ import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Lodging from "./pages/Lodging";
 import About from "./pages/About";
-import Error404 from "./components/Error404";
-
+import Error404 from "./pages/Error404";
+import React, {useState, useEffect} from "react";
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('logements.json'
+      , {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+       setItems(data)
+      });
+  }, [])
+
+
   return (
-    <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/lodging" element={<Lodging />} />
-        <Route path="/about" element={<About />} />
-        <Route element={<Error404 />} />
-    </Routes>
+    <div className="App">
+      <Routes>
+        <Route path="" element={<Home data={items}/>}/>
+        <Route path="/lodging" element={<Lodging/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route element={<Error404/>}/>
+      </Routes>
+    </div>
   );
 }
 
