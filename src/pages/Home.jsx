b3fbffd396b8654b827d2c91a6d9aed762aed904
LauNM <laurie.beduneau@gmail.com>
2022-06-22 16:31:35 +0200

    added cover image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import Thumb from "../components/Thumb/Thumb";
+import coverImage from "../assets/images/background-image.png";
 
 function Home({data}) {
   return (
     <div className={"main"}>
+      <div className={"home-header"}>
+        <div className={"cover-gradient"}/>
+        <img src={coverImage} alt={'cover-image'} className={'cover-home-page'}/>
+        <h1 className={"home-title"}>Chez vous, partout et ailleurs</h1>
+      </div>
        <div className={"thumb-wrapper"}>
        {data.map((item, i) =>
           <Thumb
